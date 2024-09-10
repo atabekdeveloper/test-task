2896e6043ff530994543efc7a97c79c56eb5fc2c
koujidev <atabek122002@gmail.com>
2024-09-11 00:23:55 +0500

    Fix bug

--- src/components/pages/Login/index.tsx
+++ src/components/pages/Login/index.tsx
@@ -77,7 +77,7 @@ export const Login: React.FC = () => {
             <button
               type="submit"
               className="flex items-center justify-center w-full px-4 py-2 text-sm font-medium text-white bg-indigo-600 rounded-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-              disabled={isLoading} // Отключить кнопку при загрузке
+              disabled={isLoading}
             >
               {isLoading ? (
                 <svg
